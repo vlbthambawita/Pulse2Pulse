3bbb41fdd632ee56ea87a69ab2c729a5c7e90754
vlbthambawita <vlbthambawita@gmail.com>
2021-06-05 12:26:23 +0200

    first commit

---
+++ data/ECGDataLoader_RHTM_gt_code_only_normal_10s.py
@@ -0,0 +1,190 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+#====== Only for testing ========================
+# data_root = "/home/vajira/DL/ecg_test/ECG_data/medians"
+# gt_csv = "/home/vajira/DL/ecg_test/ECG_data/ground_truth.csv"
+# columns_to_return_gt = ["QOnset", "QOffset", "POnset", "POffset", "TOffset"] 
+
+data_csv = [#"/home/vajira/ecg/GE/asc/ground_truth_updated_vajira.csv", 
+ "/home/vajira/ecg/gesus/asc/ground_truth_retired190424_updated.csv",  "/home/vajira/ecg/int99/asc/ground_truth_retired190424_updated.csv"]
+data_root = [#"/home/vajira/ecg/GE/asc/medians",  
+"/home/vajira/ecg/gesus/asc/medians", "/home/vajira/ecg/int99/asc/medians"]
+columns_to_return = ["P_RInterval","QRSDuration",  "Q_TInterval",	"QOnset", "QOffset", "POnset", "POffset", "TOffset"]
+
+class ECGDataALL(Dataset):
+    def __init__(self, csv_files, root_dirs, columns_to_return_gt,  transform=None):
+        self.dfs = []
+        
+        for i in range(len(csv_files)):
+            df = pd.read_csv(csv_files[i], sep=";") 
+            df["root_dir"] = root_dirs[i]
+            self.dfs.append(df)
+        
+        self.df_gt = pd.concat(self.dfs, ignore_index=True, sort=False)  # concatenate all data frames
+        self.transform = transform
+        self.columns_to_return = columns_to_return_gt
+
+        self.df_gt = self.df_gt.loc[(self.df_gt["ECGCategory"] == "Normal ECG")] # | (self.df_gt["ECGCategory"] == "Otherwise normal ECG")]
+
+        self.df_gt = self.df_gt.dropna(subset=columns_to_return_gt) # droped NaN values
+        #self.noise = 0.00000001 # noise to remove floating point exception
+        print(self.df_gt.head())
+        print(self.df_gt.tail())
+
+    def __len__(self):
+        return len(self.df_gt)
+
+    def __getitem__(self, idx):
+        
+        # Get file name of asc file
+        asc_file_name = os.path.join(self.df_gt.iloc[idx]["root_dir"],
+                                str(self.df_gt.iloc[idx]["TestID"]) + ".asc")
+        # print("asc file name=", asc_file_name)
+        
+        ecg_signals = pd.read_csv(asc_file_name,header=None, sep=" ") # read into dataframe
+        ecg_signals = torch.tensor(ecg_signals.values) # convert dataframe values to tensor
+        
+        # normalization
+        ecg_signals = ecg_signals.float()
+        #print(ecg_signals)
+        
+        #print(ecg_signals.shape)
+        ecg_signals = ecg_signals  / 6000
+
+        #cropping
+        ecg_signals = ecg_signals #[0:4096, :]
+        
+        # Transposing the ecg signals
+        ecg_signals = ecg_signals.t() 
+        #ecg_signals = ecg_signals.unsqueeze(0)
+
+       
+        
+        # reshaping to 1 x 4800 tensor
+        #ecg_signals = torch.reshape(ecg_signals, (-1,))
+        
+        #print("float ecg data")
+        #print(ecg_signals)
+        
+        
+        gt = self.df_gt.iloc[idx][self.columns_to_return].values
+        #print("gt==", gt)
+        
+        gt_code = np.zeros(5000) # GT code 
+
+        for gt_value in gt:
+            # print("gt value:", gt_value)
+            gt_code[int(gt_value)] = 1 # gt_code set to 1 for specific locations
+
+        # normalized gt
+        gt = np.array(gt, dtype=np.float)#gt.float()
+        gt = gt  # / 600 # length of the time line is 600
+       # print(gt)
+        
+        ########################################################
+        # Generating fake ground truth for conditional GAN
+        # Based on the rescaled ground truth
+        # order of GT => "QOnset", "QOffset", "POnset", "POffset", "TOffset"
+        #######################################################
+        # Calculated mean and std from all data
+        # q_on_set: 	 std = 0.007515963146052018 	 mean = 0.36299921304668137 
+        # q_off_set: 	 std = 0.009924477906686631 	 mean = 0.44070541155984194 
+        # p_on_set: 	 std = 0.02148843929437315 	     mean = 0.2302298071127185 
+        # p_off_set: 	 std = 0.020066791129587362 	 mean = 0.31901371308016874 
+        # t_off_set: 	 std = 0.02405158031032503 	     mean = 0.7015729857343781 
+        
+         # ! these are completely wron  calculations
+        # q_on_set: 	 std = 23.616443916502067 	 mean = 159.3095238095238 
+        # q_off_set: 	 std = 12.759898314265914 	 mean = 93.24743821579264 
+        # p_on_set: 	 std = 28.59356673186787 	 mean = 406.28852722523607 
+        # p_off_set: 	 std = 4.509577887631211 	 mean = 217.79952782800885 
+        # t_off_set: 	 std = 5.95468674401198 	 mean = 264.4232469359052 
+
+        # Todo Update using these values
+        # q_on_set: 	 std = 4.509577887631211 	 mean = 217.79952782800885 
+        # q_off_set: 	 std = 5.95468674401198 	 mean = 264.4232469359052 
+        # p_on_set: 	 std = 12.893063576623891 	 mean = 138.1378842676311 
+        # p_off_set: 	 std = 12.040074677752418 	 mean = 191.40822784810126 
+        # t_off_set: 	 std = 14.430948186195016 	 mean = 420.9437914406269 
+        ##########################################################
+        ##########################################################
+        
+        # Previous used cases: May be wrong
+        # gen_gt_q_on_set = np.random.normal(0.3630, 0.0075, 1)
+        # gen_gt_q_off_set = np.random.normal(0.4407, 0.0099, 1)
+        # gen_gt_p_on_set = np.random.normal(0.2302, 0.0215, 1)
+        # gen_gt_p_off_set = np.random.normal(0.3190, 0.0201, 1)
+        # gen_gt_t_off_set = np.random.normal(0.7016, 0.0241, 1)
+        
+        gen_gt_q_on_set = int(np.random.normal(217.79952782800885, 4.509577887631211,  1))
+        gen_gt_q_off_set = int(np.random.normal(264.4232469359052, 5.95468674401198 ,  1))
+        gen_gt_p_on_set = int(np.random.normal(138.1378842676311, 12.893063576623891,  1))
+        gen_gt_p_off_set = int(np.random.normal(191.40822784810126 , 12.040074677752418,  1))
+        gen_gt_t_off_set = int(np.random.normal(420.9437914406269, 14.430948186195016,  1))
+        
+        # all in one array
+        gen_gt = [gen_gt_q_on_set, gen_gt_q_off_set, gen_gt_p_on_set, 
+                  gen_gt_p_off_set, gen_gt_t_off_set]
+        gen_gt = np.asarray(gen_gt)
+
+        fake_gt_code = np.zeros(5000)
+
+        for gen_gt_value in gen_gt:
+            fake_gt_code[gen_gt_value] = 1 
+       
+       # print(type(gt))
+       # print(type(gen_gt))
+        
+        # Return sample at the end
+        sample = {'ecg_signals': ecg_signals, 'gt': gt, 'gen_gt': gen_gt, 'gt_code': gt_code, 'fake_gt_code': fake_gt_code}
+
+        if self.transform:
+            pass
+            #sample = self.transform(sample["ecg_signals"])
+
+        return sample
+    
+    
+if __name__ == "__main__":
+    
+    data_roots = [#"/home/vajira/ecg/GE/asc/medians", 
+              "/home/vajira/ecg/gesus/asc/rhythm", 
+             "/home/vajira/ecg/int99/asc/rhythm"
+              ]
+    gt_csvs = [#"/home/vajira/ecg/GE/asc/ground_truth.csv", 
+           "/home/vajira/ecg/gesus/asc/ground_truth.csv",
+          "/home/vajira/ecg/int99/asc/ground_truth.csv"
+           ]
+
+    gt_csvs_new = [#"/home/vajira/ecg/GE/asc/ground_truth.csv", 
+           "/home/vajira/DL/v243_ground_truth/gesus/ground_truth.csv",
+          "/home/vajira/DL/v243_ground_truth/int99/ground_truth.csv"
+           ]
+
+    columns_to_return_gt = ["QOnset", "QOffset", "POnset", "POffset", "TOffset"] 
+    
+    dataset = ECGDataALL(gt_csvs_new, data_roots, columns_to_return_gt)
+    print("dataset_size:", len(dataset))
+    print("df_gt", dataset.df_gt.head())
+# save to csv
+    dataset.df_gt.to_csv("gan_trained_data.csv", index=False)
+
+    t =dataset[1]
+    print(t)
+    print("ecg shape:", t["ecg_signals"].shape)
+    print("gt shape:", t["gt"].shape)
+    print("GT:",t["gt"])
+    print("fake GT:",t["gen_gt"])
+    gt = t["gt"].astype(int)
+    print(gt)
+    #embed = nn.Embedding(600, 100)
+    #gt_embed = embed(torch.from_numpy(gt))
+    #print(gt_embed)
+   # c = gt_embed.reshape(-1)
+   # print(c.shape)
---
+++ pulse2pulse.py
@@ -0,0 +1,419 @@
+#===========================================
+# Reference: https://github.com/mazzzystar/WaveGAN-pytorch
+#===========================================
+
+import argparse
+import os
+from tqdm import tqdm
+import numpy as np
+
+#Pytorch
+import torch
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.nn as nn
+from torchvision import models, transforms
+from torchvision.utils import save_image
+from torch.utils.tensorboard import SummaryWriter
+from torch.autograd import Variable
+from torchsummary import summary
+from torch import autograd
+
+
+# Model specific
+from data.ECGDataLoader_RHTM_gt_code_only_normal_10s import ECGDataALL as ecg_data
+from wave_gan import WaveGANGenerator_wave2wave_005 as WaveGANGenerator
+from wave_gan import WaveGANDiscriminator_wave2wave_005 as WaveGANDiscriminator
+from wave_gan.utils import calc_gradient_penalty, get_plots_RHTM_10s, get_plots_all_RHTM_10s
+
+torch.manual_seed(0)
+np.random.seed(0)
+parser = argparse.ArgumentParser()
+
+# Hardware
+
+parser.add_argument("--device_id", type=int, default=0, help="Device ID to run the code")
+parser.add_argument("--py_file",default=os.path.abspath(__file__)) # store current python file
+
+
+#==============================
+# Directory and file handling
+#==============================
+parser.add_argument("--data_root", default=["/home/vajira/ecg/gesus/asc/rhythm", 
+                                            "/home/vajira/ecg/int99/asc/rhythm"], help="Data roots")
+
+parser.add_argument("--data_csv", default=["/home/vajira/ecg/gesus/asc/ground_truth.csv",
+          "/home/vajira/ecg/int99/asc/ground_truth.csv"], help="Ground Truth CSVs")
+
+parser.add_argument("--columns_to_return", default=["QOnset", "QOffset", "POnset", "POffset", "TOffset"], help="coloumns to return")
+
+parser.add_argument("--out_dir", 
+                    default="/home/vajira/DL/ecggan_out",
+                    help="Main output dierectory")
+
+parser.add_argument("--tensorboard_dir", 
+                    default="/home/vajira/DL/ecg_tensorboard",
+                    help="Folder to save output of tensorboard")
+#======================
+# Hyper parameters
+#======================
+parser.add_argument("--bs", type=int, default=32, help="Mini batch size")
+parser.add_argument("--lr", type=float, default=0.0001, help="Learning rate for training")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.9, help="adam: decay of first order momentum of gradient")
+
+parser.add_argument("--num_epochs", type=int, default=4000, help="number of epochs of training")
+parser.add_argument("--start_epoch", type=int, default=0, help="Start epoch in retraining")
+parser.add_argument("--ngpus", type=int, default=1, help="Number of GPUs used in models")
+parser.add_argument("--latent_dim", type=int, default=100, help="Dimension of the latent vector")
+parser.add_argument("--checkpoint_interval", type=int, default=25, help="Interval to save checkpoint models")
+
+# Checkpoint path to retrain or test models
+parser.add_argument("--checkpoint_path", default="", help="Check point path to retrain or test models")
+
+
+parser.add_argument('-ms', '--model_size', type=int, default=50,
+                        help='Model size parameter used in WaveGAN')
+#parser.add_argument('-ms_G', '--model_size_G', type=int, default=50,
+#                        help='Model size parameter used in WaveGAN G()')
+parser.add_argument('--lmbda', default=10.0, help="Gradient penalty regularization factor")
+
+# Action handling 
+parser.add_argument("action", type=str, help="Select an action to run", choices=["train", "retrain", "inference", "check"])
+
+
+opt = parser.parse_args()
+print(opt)
+
+#==========================================
+# Device handling
+#==========================================
+torch.cuda.set_device(opt.device_id)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print("device=", device)
+
+#===========================================
+# Folder handling
+#===========================================
+
+#make output folder if not exist
+os.makedirs(opt.out_dir, exist_ok=True)
+
+
+# make subfolder in the output folder 
+py_file_name = opt.py_file.split("/")[-1] # Get python file name (soruce code name)
+checkpoint_dir = os.path.join(opt.out_dir, py_file_name + "/checkpoints")
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+# make tensorboard subdirectory for the experiment
+tensorboard_exp_dir = os.path.join(opt.tensorboard_dir, py_file_name)
+os.makedirs( tensorboard_exp_dir, exist_ok=True)
+
+
+
+#==========================================
+# Tensorboard
+#==========================================
+# Initialize summary writer
+writer = SummaryWriter(tensorboard_exp_dir)
+
+
+#==========================================
+# Prepare Data
+#==========================================
+def prepare_data():
+    dataset =  ecg_data(opt.data_csv, opt.data_root, opt.columns_to_return)
+    
+    dataloader = torch.utils.data.DataLoader( dataset,
+        batch_size=opt.bs,
+        shuffle=True,
+        num_workers=8
+    )
+
+    return dataloader
+
+#===============================================
+# Prepare models
+#===============================================
+
+def prepare_model():
+    netG = WaveGANGenerator(model_size=opt.model_size, ngpus=opt.ngpus, latent_dim=opt.latent_dim, upsample=True)
+    netD = WaveGANDiscriminator(model_size=opt.model_size, ngpus=opt.ngpus)
+
+    netG = netG.to(device)
+    netD = netD.to(device)
+
+
+    return netG, netD
+
+#====================================
+# Run training process
+#====================================
+def run_train():
+    netG, netD = prepare_model()
+    optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+
+    dataloaders = prepare_data() 
+    train(netG, netD, optimizerG, optimizerD, dataloaders)
+
+def train(netG, netD, optimizerG, optimizerD, dataloader):
+
+    #train_iter = iter(dataloader)
+
+    for epoch in tqdm(range(opt.start_epoch + 1, opt.start_epoch + opt.num_epochs + 1)):
+
+        len_dataloader = len(dataloader)
+        #print("Length of Dataloader:", len_dataloader)
+
+        train_G_flag = False
+        D_cost_train_epoch = []
+        D_wass_train_epoch = []
+        G_cost_epoch = []
+    
+        for i, sample in tqdm(enumerate(dataloader, 0)):
+
+            if (i+1) % 5 == 0:
+                train_G_flag = True
+
+
+
+            # Set Discriminator parameters to require gradients.
+            #print(train_G_flag)
+            for p in netD.parameters():
+                p.requires_grad = True
+
+            #one = torch.Tensor([1]).float()
+            one = torch.tensor(1, dtype=torch.float)
+            neg_one = one * -1
+
+            one = one.to(device)
+            neg_one = neg_one.to(device)
+
+            #############################
+            # (1) Train Discriminator
+            #############################
+            
+            #real_ecgs = sample["ecg_signals"]
+            #b_size = real_ecgs.size(0)
+
+            #for iter_dis in range(5):
+                
+            #sample = next(train_iter)
+
+            real_ecgs = sample["ecg_signals"].to(device)
+            #print("real ecgs shape", real_ecgs.shape)
+            b_size = real_ecgs.size(0)
+
+            netD.zero_grad()
+
+            # Noise
+            noise = torch.Tensor(b_size, 8, 5000).uniform_(-1, 1)
+            noise = noise.to(device)
+            noise_Var = Variable(noise, requires_grad=False)
+
+            # real_data_Var = numpy_to_var(next(train_iter)['X'], cuda)
+
+            # a) compute loss contribution from real training data
+            D_real = netD(real_ecgs)
+            D_real = D_real.mean()  # avg loss
+            D_real.backward(neg_one)  # loss * -1
+
+            # b) compute loss contribution from generated data, then backprop.
+            fake = autograd.Variable(netG(noise_Var).data)
+            D_fake = netD(fake)
+            D_fake = D_fake.mean()
+            D_fake.backward(one)
+
+            # c) compute gradient penalty and backprop
+            gradient_penalty = calc_gradient_penalty(netD, real_ecgs,
+                                                    fake.data, b_size, opt.lmbda,
+                                                    use_cuda=True)
+            gradient_penalty.backward(one)
+
+            # Compute cost * Wassertein loss..
+            D_cost_train = D_fake - D_real + gradient_penalty
+            D_wass_train = D_real - D_fake
+
+            # Update gradient of discriminator.
+            optimizerD.step()
+
+            D_cost_train_cpu = D_cost_train.data.cpu()
+            D_wass_train_cpu = D_wass_train.data.cpu()
+
+            D_cost_train_epoch.append(D_cost_train_cpu)
+            D_wass_train_epoch.append(D_wass_train_cpu)
+
+
+            #############################
+            # (3) Train Generator
+            #############################
+            if train_G_flag:
+                # Prevent discriminator update.
+                for p in netD.parameters():
+                    p.requires_grad = False
+
+                # Reset generator gradients
+                netG.zero_grad()
+
+                # Noise
+                noise = torch.Tensor(b_size, 8, 5000).uniform_(-1, 1)
+                
+                noise = noise.to(device)
+                noise_Var = Variable(noise, requires_grad=False)
+
+                fake = netG(noise_Var)
+                G = netD(fake)
+                G = G.mean()
+
+                # Update gradients.
+                G.backward(neg_one)
+                G_cost = -G
+
+                optimizerG.step()
+
+                # Record costs
+                #if cuda:
+                G_cost_cpu = G_cost.data.cpu()
+                G_cost_epoch.append(G_cost_cpu)
+                #print("Epoch{} - {}_G_cost_cpu:{}".format(epoch, i, G_cost_cpu))
+                #G_cost_epoch.append(G_cost_cpu.data.numpy())
+                train_G_flag =False
+
+                #print("real ecg:", real_ecgs.shape)
+                #print("fake ecg:", fake.shape)
+            if i == 0: # take the first batch to plot
+                real_ecgs_to_plot = real_ecgs
+                fake_to_plot = fake
+            #    break
+        
+        D_cost_train_epoch_avg = sum(D_cost_train_epoch) / float(len(D_cost_train_epoch))
+        D_wass_train_epoch_avg = sum(D_wass_train_epoch) / float(len(D_wass_train_epoch))
+        G_cost_epoch_avg = sum(G_cost_epoch) / float(len(G_cost_epoch))
+
+        
+        writer.add_scalar("D_cost_train_epoch_avg",D_cost_train_epoch_avg ,epoch)
+        writer.add_scalar("D_wass_train_epoch_avg",D_wass_train_epoch_avg ,epoch)
+        writer.add_scalar("G_cost_epoch_avg ",G_cost_epoch_avg  ,epoch)
+
+        print("Epochs:{}\t\tD_cost:{}\t\t D_wass:{}\t\tG_cost:{}".format(
+                    epoch, D_cost_train_epoch_avg, D_wass_train_epoch_avg, G_cost_epoch_avg))
+
+         # Save model
+        if epoch % opt.checkpoint_interval == 0:
+            save_model(netG, netD, optimizerG, optimizerD, epoch)
+            fig = get_plots_RHTM_10s(real_ecgs_to_plot[0].detach().cpu(), fake_to_plot[0].detach().cpu())
+            fig_2 = get_plots_all_RHTM_10s(real_ecgs_to_plot.detach().cpu(), fake_to_plot.detach().cpu())
+
+            writer.add_figure("sample", fig, epoch)
+            writer.add_figure("sample_batch", fig_2, epoch)
+        #fig.savefig("{}.png".format(epoch))
+
+
+#=====================================
+# Save models
+#=====================================
+def save_model(netG, netD, optimizerG, optimizerD,  epoch):
+   
+    check_point_name = py_file_name + "_epoch:{}.pt".format(epoch) # get code file name and make a name
+    check_point_path = os.path.join(checkpoint_dir, check_point_name)
+    # save torch model
+    torch.save({
+        "epoch": epoch,
+        "netG_state_dict": netG.state_dict(),
+        "netD_state_dict": netD.state_dict(),
+        "optimizerG_state_dict": optimizerG.state_dict(),
+        "optimizerD_state_dict": optimizerD.state_dict(),
+        # "train_loss": train_loss,
+        #"val_loss": validation_loss
+    }, check_point_path)
+
+#====================================
+# Re-train process
+#====================================
+def run_retrain():
+    print("run retrain started........................")
+    netG, netD = prepare_model()
+
+    #netG.cpu()
+    #netD.cpu()
+
+    # loading checkpoing
+    chkpnt = torch.load(opt.checkpoint_path, map_location="cpu")
+
+    netG.load_state_dict(chkpnt["netG_state_dict"])
+    netD.load_state_dict(chkpnt["netD_state_dict"])
+
+    netG = netG.to(device)
+    netD = netD.to(device)
+
+    print("model loaded from checkpoint=", opt.checkpoint_path)
+
+    # setup start epoch to checkpoint epoch
+    opt.__setattr__("start_epoch", chkpnt["epoch"])
+
+    optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+
+    dataloaders = prepare_data()
+    train(netG, netD, optimizerG, optimizerD, dataloaders)
+    
+
+#=====================================
+# Check model
+#====================================
+def check_model_graph():
+    netG, netD = prepare_model()
+
+    #netG = netG.cpu()
+    #netD = netD.cpu()
+
+    #summary(model, (9, 256, 256)) # this run on GPU
+    #model = model.to('cpu')
+    #dataloaders = prepare_data()
+    #sample = next(iter(dataloaders["train"]))
+
+    #inputs = sample["features"]
+   # inputs = inputs.to(device, torch.float)
+    #print(inputs.shape)
+    print(netG)
+    netG = netG.cuda()
+    netD = netD.cuda()
+   # print(netD)
+    #netG = netG.to("cpu")
+    #dummy_input = Variable(torch.rand(13, 90, 64, 64))
+    summary(netG, (8,5000))
+    summary(netD, (8, 5000))
+    #dummy_input = Variable(torch.rand(13, 90, 64, 64))
+    
+    #writer.add_graph(model, dummy_input) # this need the model on CPU
+
+
+
+if __name__ == "__main__":
+
+    data_loaders = prepare_data()
+    print(vars(opt))
+    print("Test OK")
+
+    # Train or retrain or inference
+    if opt.action == "train":
+        print("Training process is strted..!")
+        run_train()
+        pass
+    elif opt.action == "retrain":
+        print("Retrainning process is strted..!")
+        run_retrain()
+        pass
+    elif opt.action == "inference":
+        print("Inference process is strted..!")
+        pass
+    elif opt.action == "check":
+        check_model_graph()
+        print("Check pass")
+
+    # Finish tensorboard writer
+    writer.close()
